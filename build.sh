c6ab4d0acce346759b1bff280300afb4cd27fe36
Jamie R. Oaks <joaks1@gmail.com>
2022-08-26 12:09:41 -0500

    Improving builds script.
    Improving clean-up logic in build script.

--- build.sh
+++ build.sh
@@ -2,20 +2,24 @@
 
 set -e
 
+
 # Get path to directory of this script
 package_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+src_dir="${package_dir}/pt"
 
-# Clear out any previously compiled files
-if [ -n "$(find "${package_dir}/pt" -name "*.class")" ]
-then
-    rm $(find "${package_dir}/pt" -name "*.class")
-fi
+# Function to remove compiled class files
+clean_up() {
+    if [ -n "$(find "$src_dir" -name "*.class")" ]
+    then
+        rm $(find "$src_dir" -name "*.class")
+    fi
+}
+# Make sure we clean up class files on exit
+trap clean_up EXIT
 
+clean_up
 (
     cd "$package_dir"
     javac pt/ua/ieeta/RNAmfeOpt/main/Main.java
     jar cvfm mRNAOptimiser.jar manifest.txt pt/ua/ieeta/RNAmfeOpt/*/*.class
 )
-
-# Clean up class files
-rm $(find "${package_dir}/pt" -name "*.class")
